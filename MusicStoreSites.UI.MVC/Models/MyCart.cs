b721776a9258e07839ea528a765fc44ef94e41ae
Pınar Sarıkoç <41378201+pinarsarikoc@users.noreply.github.com>
2019-07-17 16:49:23 +0300

    Initial commit

---
+++ MusicStoreSites.UI.MVC/Models/MyCart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStoreSites.UI.MVC.Models
+{
+    public class MyCart
+    {
+        private Dictionary<int, CartItemDTO> _sepet = new Dictionary<int, CartItemDTO>();
+
+        public List<CartItemDTO> GetAllCartItem
+        {
+            get
+            {
+                return _sepet.Values.ToList();
+            }
+        }
+
+        public void Add(CartItemDTO cartitemDTO)
+        {
+            if (_sepet.ContainsKey(cartitemDTO.ID))
+            {
+                _sepet[cartitemDTO.ID].Amount += cartitemDTO.Amount;
+                return;
+            }
+            _sepet.Add(cartitemDTO.ID, cartitemDTO);
+        }
+
+        public void Update(int id,short amount)
+        {
+            if (_sepet.ContainsKey(id))
+            {
+                _sepet[id].Amount = amount;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            if (_sepet.ContainsKey(id))
+            {
+                _sepet.Remove(id);
+            }
+        }
+
+        public int TotalAmount
+        {
+            get
+            {
+                return _sepet.Values.Sum(x => x.Amount);
+            }
+        }
+    }
+}
